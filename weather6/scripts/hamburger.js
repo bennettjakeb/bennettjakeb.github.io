5864ab43b90ae20185aeb9c92e2a4ddd6c8f7eec
Jake Bennett Account <bennettjakeb@gmail.com>
2019-02-16 13:40:26 -0600

    made the hamburger java work perfectly

--- weather6/scripts/hamburger.js
+++ weather6/scripts/hamburger.js
@@ -1,3 +1,4 @@
 function toggleMenu(){
-    console.log("it worked");
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
 }
