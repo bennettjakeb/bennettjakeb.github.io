f8a44810a8b10218086cf5220ce3a0b7bc051ecf
Jake Bennett Account <bennettjakeb@gmail.com>
2019-03-17 14:53:36 -0500

    updated fiveday.js

--- lesson-10/scripts/fiveday.js
+++ lesson-10/scripts/fiveday.js
@@ -8,29 +8,29 @@ forcast.onload = function() {
     console.log(forcastInfo);
 
 
-    document.getElementById('description1').innerHTML = forcastInfo.list[4].weather["0"].description;
-    document.getElementById('description2').innerHTML = forcastInfo.list[12].weather["0"].description;
-    document.getElementById('description3').innerHTML = forcastInfo.list[20].weather["0"].description;
-    document.getElementById('description4').innerHTML = forcastInfo.list[28].weather["0"].description;
-    document.getElementById('description5').innerHTML = forcastInfo.list[36].weather["0"].description;
+    document.getElementById('description1').innerHTML = forcastInfo.list[5].weather["0"].description;
+    document.getElementById('description2').innerHTML = forcastInfo.list[13].weather["0"].description;
+    document.getElementById('description3').innerHTML = forcastInfo.list[21].weather["0"].description;
+    document.getElementById('description4').innerHTML = forcastInfo.list[29].weather["0"].description;
+    document.getElementById('description5').innerHTML = forcastInfo.list[37].weather["0"].description;
 
 
 
-    document.getElementById('low1').innerHTML = forcastInfo.list[4].main.temp_min;
-    document.getElementById('low2').innerHTML = forcastInfo.list[12].main.temp_min;
-    document.getElementById('low3').innerHTML = forcastInfo.list[20].main.temp_min;
-    document.getElementById('low4').innerHTML = forcastInfo.list[28].main.temp_min;
-    document.getElementById('low5').innerHTML = forcastInfo.list[36].main.temp_min;
+    document.getElementById('low1').innerHTML = forcastInfo.list[5].main.temp_min;
+    document.getElementById('low2').innerHTML = forcastInfo.list[13].main.temp_min;
+    document.getElementById('low3').innerHTML = forcastInfo.list[21].main.temp_min;
+    document.getElementById('low4').innerHTML = forcastInfo.list[29].main.temp_min;
+    document.getElementById('low5').innerHTML = forcastInfo.list[37].main.temp_min;
 
-    document.getElementById('high1').innerHTML = forcastInfo.list[4].main.temp_max;
-    document.getElementById('high2').innerHTML = forcastInfo.list[12].main.temp_max;
-    document.getElementById('high3').innerHTML = forcastInfo.list[20].main.temp_max;
-    document.getElementById('high4').innerHTML = forcastInfo.list[28].main.temp_max;
-    document.getElementById('high5').innerHTML = forcastInfo.list[36].main.temp_max;
+    document.getElementById('high1').innerHTML = forcastInfo.list[5].main.temp_max;
+    document.getElementById('high2').innerHTML = forcastInfo.list[13].main.temp_max;
+    document.getElementById('high3').innerHTML = forcastInfo.list[21].main.temp_max;
+    document.getElementById('high4').innerHTML = forcastInfo.list[29].main.temp_max;
+    document.getElementById('high5').innerHTML = forcastInfo.list[37].main.temp_max;
 
-    document.getElementById('wind1').innerHTML = forcastInfo.list[4].wind.speed;
-    document.getElementById('wind2').innerHTML = forcastInfo.list[12].wind.speed;
-    document.getElementById('wind3').innerHTML = forcastInfo.list[20].wind.speed;
-    document.getElementById('wind4').innerHTML = forcastInfo.list[28].wind.speed;
-    document.getElementById('wind5').innerHTML = forcastInfo.list[36].wind.speed;
+    document.getElementById('wind1').innerHTML = forcastInfo.list[5].wind.speed;
+    document.getElementById('wind2').innerHTML = forcastInfo.list[13].wind.speed;
+    document.getElementById('wind3').innerHTML = forcastInfo.list[21].wind.speed;
+    document.getElementById('wind4').innerHTML = forcastInfo.list[29].wind.speed;
+    document.getElementById('wind5').innerHTML = forcastInfo.list[37].wind.speed;
 } // end of the function
