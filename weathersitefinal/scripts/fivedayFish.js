bb06d80d8b67840245813e435384ac691e32155f
Jake Bennett Account <bennettjakeb@gmail.com>
2019-03-23 09:40:59 -0500

    updated links

---
+++ weathersitefinal/scripts/fivedayFish.js
@@ -0,0 +1,36 @@
+var forcast  = new XMLHttpRequest();
+forcast.open('GET','https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=bc867b22ec7fc59aa9b901b6ef54bd32&units=imperial',true);
+
+forcast.send();
+
+forcast.onload = function() {
+    var forcastInfo = JSON.parse(forcast.responseText);
+    console.log(forcastInfo);
+
+
+    document.getElementById('description1f').innerHTML = forcastInfo.list[5].weather["0"].description;
+    document.getElementById('description2f').innerHTML = forcastInfo.list[13].weather["0"].description;
+    document.getElementById('description3f').innerHTML = forcastInfo.list[21].weather["0"].description;
+    document.getElementById('description4f').innerHTML = forcastInfo.list[29].weather["0"].description;
+    document.getElementById('description5f').innerHTML = forcastInfo.list[37].weather["0"].description;
+
+
+
+    document.getElementById('low1f').innerHTML = forcastInfo.list[5].main.temp_min;
+    document.getElementById('low2f').innerHTML = forcastInfo.list[13].main.temp_min;
+    document.getElementById('low3f').innerHTML = forcastInfo.list[21].main.temp_min;
+    document.getElementById('low4f').innerHTML = forcastInfo.list[29].main.temp_min;
+    document.getElementById('low5f').innerHTML = forcastInfo.list[37].main.temp_min;
+
+    document.getElementById('high1f').innerHTML = forcastInfo.list[5].main.temp_max;
+    document.getElementById('high2f').innerHTML = forcastInfo.list[13].main.temp_max;
+    document.getElementById('high3f').innerHTML = forcastInfo.list[21].main.temp_max;
+    document.getElementById('high4f').innerHTML = forcastInfo.list[29].main.temp_max;
+    document.getElementById('high5f').innerHTML = forcastInfo.list[37].main.temp_max;
+
+    document.getElementById('wind1f').innerHTML = forcastInfo.list[5].wind.speed;
+    document.getElementById('wind2f').innerHTML = forcastInfo.list[13].wind.speed;
+    document.getElementById('wind3f').innerHTML = forcastInfo.list[21].wind.speed;
+    document.getElementById('wind4f').innerHTML = forcastInfo.list[29].wind.speed;
+    document.getElementById('wind5f').innerHTML = forcastInfo.list[37].wind.speed;
+} // end of the function
---
+++ weathersitefinal/scripts/weatherFish.js
@@ -0,0 +1,23 @@
+var weatherObject  = new XMLHttpRequest();
+weatherObject.open('GET','https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=bc867b22ec7fc59aa9b901b6ef54bd32&units=imperial',true);
+
+weatherObject.send();
+
+weatherObject.onload = function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('place').innerHTML = weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
+    document.getElementById('humid').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('currently').innerHTML = weatherInfo.weather[0].description;
+
+    var temp = parseFloat(document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp);
+    var wind = parseFloat(document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed);
+    var speed = Math.pow(wind,.16)
+    var answer = (35.74 + (0.6215*temp)) - (35.75*speed) + (0.4275*temp*speed)
+
+    document.getElementById("windChill").innerHTML = Math.round(answer);
+
+} // end of the function
