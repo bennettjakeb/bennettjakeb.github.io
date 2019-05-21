14411999bad88b5e3a83a447a9b369002086cc0c
Jake Bennett Account <bennettjakeb@gmail.com>
2019-05-21 18:05:27 -0500

    moved javascript to other file

---
+++ CIT261java/CSS.JS.js
@@ -0,0 +1,20 @@
+function myFunction(){
+    // document.getElementById(id name).style.property = new style;
+
+    // chnage the color of the text
+    document.getElementById("para").style.color="blue";
+    // align the text to the right
+    document.getElementById("para").style.textAlign="right";
+    // change the size of the text to 3em, rather than 16px
+    document.getElementById("para").style.fontSize="3em";
+}
+
+function visible(){
+    // this code makes the text visable
+    document.getElementById("para2").style.visibility="visible";
+}
+
+function invisible(){
+    // this code makes the text dissapear
+    document.getElementById("para2").style.visibility="hidden";
+}
